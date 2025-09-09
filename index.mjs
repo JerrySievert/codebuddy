774862b695847fadc2c1a414e8ad68a1de7b1222
Jerry Sievert <code@legitimatesounding.com>
2025-09-09 14:14:45 -0700

    code snapshot

--- index.mjs
+++ index.mjs
@@ -2,23 +2,25 @@ import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
 import { z } from 'zod';
 
+import { get_all_projects_with_metadata } from './lib/model/project.mjs';
 import {
-  get_all_projects_with_metadata,
-  get_project
-} from './lib/model/project.mjs';
-import { get_entity } from './lib/model/entity.mjs';
+  get_entity,
+  get_entity_symbols,
+  entity_search
+} from './lib/model/entity.mjs';
 import {
   build_call_tree,
   get_entities_by_caller_id,
-  get_entities_by_calling_id
+  get_entities_by_callee_id
 } from './lib/model/relationship.mjs';
 import { get_sourcecode } from './lib/model/sourcecode.mjs';
 import { text_at_position } from './lib/sourcecode.mjs';
+import { get_project_by_name } from './lib/model/project.mjs';
 
 import { tools } from './lib/strings.mjs';
 
 const server = new McpServer({
-  name: 'code-knowledge-graph',
+  name: 'codebuddy',
   version: '1.0.0'
 });
 
@@ -36,38 +38,147 @@ const check_arguments = (args, required) => {
 };
 
 server.tool(
-  tools[`retrieve_function`].name,
-  tools['retrieve_function'].description,
+  tools[`function_list`].name,
+  tools[`function_list`].description,
   {
-    function_name: z.string().describe('Name of the function to retrieve'),
-    project_id: z
-      .number()
+    project: z
+      .string()
+      .describe('Project name to retrieve the function list from'),
+    filename: z
+      .string()
       .optional()
-      .describe('Project ID to retrieve function from')
+      .describe('Filename to retrieve the function list from')
   },
-  async ({ function_name, project_id, type }) => {
+  async ({ project, filename }) => {
     try {
-      const entities = await get_entity({
-        synbol: function_name,
-        project_id,
-        type
+      const projects = await get_project_by_name({ name: project });
+
+      if (projects.length === 0) {
+        throw new Error(`Project '${project}' not found`);
+      }
+
+      const symbols = await get_entity_symbols({
+        project_id: projects[0].id,
+        filename,
+        type: 'function'
       });
 
       const content = [];
-      for (const entity of entities) {
-        const sources = await get_sourcecode({
-          project_id: entity.project_id,
-          filename: entity.filename
+      for (const symbol of symbols) {
+        content.push({
+          type: 'text',
+          text: JSON.stringify(symbol)
         });
+      }
 
-        if (sources.length) {
-          entity.source = text_at_position({
-            source: sources[0].source,
-            start_line: entity.start_line,
-            end_line: entity.end_line
-          });
+      return {
+        content: content
+      };
+    } catch (err) {
+      return {
+        content: [{ type: 'text', text: `Error ${err}` }],
+        isError: true
+      };
+    }
+  }
+);
+
+server.tool(
+  tools[`function_search`].name,
+  tools[`function_search`].description,
+  {
+    name: z.string().describe('Name of the function to search for'),
+    project: z
+      .string()
+      .optional()
+      .describe('Project name in which to search for the function'),
+    filename: z
+      .string()
+      .optional()
+      .describe('Filename in which to search for the function'),
+    limit: z
+      .number()
+      .optional()
+      .default(10)
+      .describe('Maximum number of results to return')
+  },
+  async ({ name, project, filename, limit }) => {
+    try {
+      let project_id;
+      if (project !== undefined) {
+        const projects = await get_project_by_name({ name: project });
+
+        if (projects.length !== 0) {
+          project_id = projects[0].id;
         }
+      }
 
+      const symbols = await entity_search({
+        project_id,
+        filename,
+        symbol: name,
+        type: 'function',
+        limit
+      });
+
+      if (symbols.length === 0) {
+        throw new Error(`Function '${name}' not found`);
+      }
+
+      const content = [];
+      for (const symbol of symbols) {
+        content.push({
+          type: 'text',
+          text: JSON.stringify(symbol)
+        });
+      }
+
+      return {
+        content: content
+      };
+    } catch (err) {
+      return {
+        content: [{ type: 'text', text: `Error ${err}` }],
+        isError: true
+      };
+    }
+  }
+);
+
+server.tool(
+  tools[`function_retrieve`].name,
+  tools['function_retrieve'].description,
+  {
+    name: z.string().describe('Name of the function to retrieve'),
+    project: z
+      .string()
+      .optional()
+      .describe('Project name to retrieve function from')
+  },
+  async ({ name, project, filename }) => {
+    try {
+      let project_id;
+      if (project !== undefined) {
+        const projects = await get_project_by_name({ name: project });
+
+        if (projects.length !== 0) {
+          project_id = projects[0].id;
+        }
+      }
+
+      const entities = await get_entity({
+        project_id,
+        filename,
+        symbol: name,
+        type: 'function'
+      });
+
+      if (entities.length === 0) {
+        throw new Error(`Function '${name}' not found`);
+      }
+
+      const content = [];
+      for (const entity of entities) {
         content.push({
           type: 'text',
           text: JSON.stringify(entity)
@@ -118,18 +229,35 @@ server.tool(
 );
 */
 server.tool(
-  tools[`retrieve_called_functions`].name,
-  tools[`retrieve_called_functions`].description,
+  tools[`function_callers`].name,
+  tools[`function_callers`].description,
   {
-    entity_id: z
-      .number()
-      .describe(
-        'Entity ID of the symbol (function) to retrieve called functions for'
-      )
+    name: z.string().describe('Function name to retrieve callers for'),
+    project: z
+      .string()
+      .optional()
+      .describe('Project to narrow the scope of the callers')
   },
-  async ({ entity_id }) => {
+  async ({ name, project }) => {
     try {
-      const entities = await get_entities_by_caller_id(entity_id);
+      let project_id;
+      if (project !== undefined) {
+        const projects = await get_project_by_name({ name: project });
+
+        if (projects.length !== 0) {
+          project_id = projects[0].id;
+        }
+      }
+
+      const entities = await get_entities_by_callee_id({
+        project_id,
+        symbol: name,
+        type: 'function'
+      });
+
+      if (entities.length === 0) {
+        throw new Error(`Function '${name}' not found`);
+      }
 
       const content = [];
       for (const entity of entities) {
@@ -152,18 +280,35 @@ server.tool(
 );
 
 server.tool(
-  tools[`retrieve_calling_functions`].name,
-  tools[`retrieve_calling_functions`].description,
+  tools[`function_callees`].name,
+  tools[`function_callees`].description,
   {
-    entity_id: z
-      .number()
-      .describe(
-        'Entity ID of the symbol (function) to retrieve calling functions for'
-      )
+    name: z.string().describe('Function name to retrieve callees for'),
+    project: z
+      .string()
+      .optional()
+      .describe('Project to narrow the scope of the callees')
   },
-  async ({ entity_id }) => {
+  async ({ name, project }) => {
     try {
-      const entities = await get_entities_by_calling_id(entity_id);
+      let project_id;
+      if (project !== undefined) {
+        const projects = await get_project_by_name({ name: project });
+
+        if (projects.length !== 0) {
+          project_id = projects[0].id;
+        }
+      }
+
+      const entities = await get_entities_by_caller_id({
+        project_id,
+        symbol: name,
+        type: 'function'
+      });
+
+      if (entities.length === 0) {
+        throw new Error(`Function '${name}' not found`);
+      }
 
       const content = [];
       for (const entity of entities) {
@@ -186,10 +331,10 @@ server.tool(
 );
 
 server.tool(
-  tools[`list_projects`].name,
-  tools['list_projects'].description,
+  tools[`project_list`].name,
+  tools['project_list'].description,
   {},
-  async ({ entity_id }) => {
+  async () => {
     try {
       const entities = await get_all_projects_with_metadata();
 
@@ -200,6 +345,36 @@ server.tool(
           text: JSON.stringify(entity)
         });
       }
+
+      return {
+        content: content
+      };
+    } catch (err) {
+      return {
+        content: [{ type: 'text', text: `Error ${err}` }],
+        isError: true
+      };
+    }
+  }
+);
+
+server.tool(
+  tools[`project_info`].name,
+  tools['project_info'].description,
+  { name: z.string().describe('Project name') },
+  async ({ name }) => {
+    try {
+      const project = await get_project_by_name({ name });
+
+      if (project.length === 0) {
+        throw new Error(`Project '${name}' not found`);
+      }
+
+      const content = [];
+      content.push({
+        type: 'text',
+        text: JSON.stringify(project[0])
+      });
 
       return {
         content: content
--- migrations/1753934080094-table_relationship.mjs
+++ migrations/1753934080094-table_relationship.mjs
@@ -5,13 +5,13 @@ import { query } from '../lib/db.mjs';
 // Execute the migration.
 const up = async () => {
   await query`
-      CREATE TABLE relationship (
-        id SERIAL PRIMARY KEY,
-        caller INT NOT NULL REFERENCES entity(id),
-        callee INT NOT NULL REFERENCES entity(id),
-        line INT NOT NULL,
-        comment TEXT
-      )`;
+    CREATE TABLE relationship (
+      id SERIAL PRIMARY KEY,
+      caller INT NOT NULL REFERENCES entity(id),
+      callee INT NOT NULL REFERENCES entity(id),
+      line INT NOT NULL,
+      comment TEXT
+    )`;
 };
 
 // Revert the migration.
